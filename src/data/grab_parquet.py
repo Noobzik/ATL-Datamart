d782d1731525103755e27e224c24c6b5d1b09029
Louis Cavecchi <cavecchilouis@gmail.com>
2024-03-14 16:58:40 +0100

    Add: add fetch data every month

--- src/data/grab_parquet.py
+++ src/data/grab_parquet.py
@@ -2,19 +2,22 @@ from minio import Minio
 import urllib.request
 import pandas as pd
 import sys
+from datetime import datetime, timedelta
 
 def main():
     grab_data()
-    
+
 
 def grab_data() -> None:
-    """Grab the data from New York Yellow Taxi
-
-    This method download x files of the New York Yellow Taxi. 
-    
-    Files need to be saved into "../../data/raw" folder
-    This methods takes no arguments and returns nothing.
-    """
+    start_date = datetime(2018, 1, 1)
+    end_date = datetime.now()
+    current_date = start_date
+    while current_date < end_date:
+        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{current_date.strftime('%Y-%m')}.parquet"
+        filename = f"../../data/raw/yellow_tripdata_{current_date.strftime('%Y-%m')}.parquet"
+        print(f"Downloading data for {current_date.strftime('%B %Y')}...")
+        urllib.request.urlretrieve(url, filename)
+        current_date = current_date.replace(month=current_date.month + 1, day=1)
 
 
 def write_data_minio():
