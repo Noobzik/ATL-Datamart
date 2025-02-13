05da28b71480d243b82a2c33d6e3acb57118e764
NimZero <contact@nimzero.fr>
2025-02-13 17:30:44 +0100

    TP1 version 1

--- src/data/grab_parquet.py
+++ src/data/grab_parquet.py
@@ -1,20 +1,33 @@
 from minio import Minio
 import urllib.request
-import pandas as pd
 import sys
 
 def main():
     grab_data()
-    
+    write_data_minio()
+
 
 def grab_data() -> None:
     """Grab the data from New York Yellow Taxi
 
-    This method download x files of the New York Yellow Taxi. 
-    
+    This method download x files of the New York Yellow Taxi.
+
     Files need to be saved into "../../data/raw" folder
     This methods takes no arguments and returns nothing.
     """
+    print("Downloading files")
+    for i in range(1, 11):
+        if i < 10:
+            i = "0" + str(i)
+        else:
+            i = str(i)
+
+        print("Downloading file yellow_tripdata_2024-" + i + ".parquet")
+        request = urllib.request.urlopen("https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-" + i + ".parquet")
+        with open("data/raw/yellow_tripdata_2024-" + i + ".parquet", "wb") as f:
+            f.write(request.read())
+
+    print("All files have been downloaded")
 
 
 def write_data_minio():
@@ -28,12 +41,25 @@ def write_data_minio():
         access_key="minio",
         secret_key="minio123"
     )
-    bucket: str = "NOM_DU_BUCKET_ICI"
+    bucket: str = "my-bucket"
     found = client.bucket_exists(bucket)
     if not found:
         client.make_bucket(bucket)
     else:
         print("Bucket " + bucket + " existe déjà")
+
+    print("Uploading files to Minio")
+    for i in range(1, 11):
+        if i < 10:
+            i = "0" + str(i)
+        else:
+            i = str(i)
+
+        print("Uploading file yellow_tripdata_2024-" + i + ".parquet")
+        client.fput_object(bucket, "yellow_tripdata_2024-" + i + ".parquet", "data/raw/yellow_tripdata_2024-" + i + ".parquet")
+
+    print("All files have been uploaded")
+
 
 if __name__ == '__main__':
     sys.exit(main())
