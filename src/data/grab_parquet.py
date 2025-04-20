13139cce2d93c1f23ac2c3952b50a504b78c7898
BenjaminAmand <jeux.amand@gmail.com>
2025-04-20 20:30:44 +0200

    TP1

--- src/data/grab_parquet.py
+++ src/data/grab_parquet.py
@@ -1,11 +1,20 @@
+import requests
 from minio import Minio
 import urllib.request
 import pandas as pd
 import sys
 
 def main():
-    grab_data()
+    get_all_data()
+    #get_last_month_data()
     
+def get_last_month_data():
+    grab_data()
+    write_data_minio()
+    
+def get_all_data():
+    grab_all_data()
+    write_all_data_minio()
 
 def grab_data() -> None:
     """Grab the data from New York Yellow Taxi
@@ -15,6 +24,28 @@ def grab_data() -> None:
     Files need to be saved into "../../data/raw" folder
     This methods takes no arguments and returns nothing.
     """
+    year = 2024
+    month = 12
+    url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month}.parquet" # fonctionne jusqu'a 2024-12, voir données dispo ici : https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page
+    response = requests.get(url)
+    with open(f'./data/raw/yellow_tripdata_{year}-{month}.parquet', 'wb') as file:
+        file.write(response.content)
+
+def grab_all_data() -> None:
+    """Grab the data from New York Yellow Taxi
+
+    This method download x files of the New York Yellow Taxi. 
+    
+    Files need to be saved into "../../data/raw" folder
+    This methods takes no arguments and returns nothing.
+    """
+    for year in range(2009,2025):
+        for month in range(1,13):
+            url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month}.parquet"
+            response = requests.get(url)
+            with open(f'./data/raw/yellow_tripdata_{year}-{month}.parquet', 'wb') as file:
+                file.write(response.content)
+    
 
 
 def write_data_minio():
@@ -28,12 +59,34 @@ def write_data_minio():
         access_key="minio",
         secret_key="minio123"
     )
-    bucket: str = "NOM_DU_BUCKET_ICI"
+    bucket: str = "bucket"
     found = client.bucket_exists(bucket)
     if not found:
         client.make_bucket(bucket)
     else:
         print("Bucket " + bucket + " existe déjà")
+    client.fput_object(bucket, "yellow_tripdata_2024-12.parquet", "./data/raw/yellow_tripdata_2024-12.parquet")
+
+def write_all_data_minio():
+    """
+    This method put all Parquet files into Minio
+    Ne pas faire cette méthode pour le moment
+    """
+    client = Minio(
+        "localhost:9000",
+        secure=False,
+        access_key="minio",
+        secret_key="minio123"
+    )
+    bucket: str = "bucket"
+    found = client.bucket_exists(bucket)
+    if not found:
+        client.make_bucket(bucket)
+    else:
+        print("Bucket " + bucket + " existe déjà")
+    for year in range(2009,2025):
+        for month in range(1,13):
+            client.fput_object(bucket, f"yellow_tripdata_{year}-{month}.parquet", f"./data/raw/yellow_tripdata_{year}-{month}.parquet")
 
 if __name__ == '__main__':
     sys.exit(main())
